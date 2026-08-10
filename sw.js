b003627e01ef7c590848683739c840141e5ac8d6
mostafaorabi987-lang <mostafaorabi987@gmail.com>
2026-08-10 16:12:55 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+/* بوابة عرابي - Service Worker (نسخة الملف الواحد) */
+const CACHE = 'arabi-plates-single-v16';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => Promise.all(ASSETS.map(u => c.add(u).catch(()=>{}))))
+      .then(() => self.skipWaiting())
+  );
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(k => Promise.all(k.filter(x => x !== CACHE).map(x => caches.delete(x))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  if (new URL(req.url).origin !== self.location.origin) return;
+  e.respondWith(
+    fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(req, copy));
+      return res;
+    }).catch(() => caches.match(req).then(r => r || caches.match('./index.html')))
+  );
+});
